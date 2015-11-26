b11667d41ae559c215c11a81e95d7f82205a0fde
Matison <matison14@gmail.com>
2015-11-26 14:28:47 +0100

    Bloki Zrobione !!!!!!!!!

--- Visual Project/Forms/Block_Add.Designer.cs
+++ Visual Project/Forms/Block_Add.Designer.cs
@@ -29,10 +29,108 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.userBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dBDataSet = new MTGCM.DBDataSet();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.userTableAdapter = new MTGCM.DBDataSetTableAdapters.UserTableAdapter();
+            this.userBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.txtBlock = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.btAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // userBindingSource
+            // 
+            this.userBindingSource.DataMember = "User";
+            this.userBindingSource.DataSource = this.dBDataSet;
+            // 
+            // dBDataSet
+            // 
+            this.dBDataSet.DataSetName = "DBDataSet";
+            this.dBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // userTableAdapter
+            // 
+            this.userTableAdapter.ClearBeforeFill = true;
+            // 
+            // userBindingSource1
+            // 
+            this.userBindingSource1.DataMember = "User";
+            this.userBindingSource1.DataSource = this.dBDataSet;
+            // 
+            // txtBlock
+            // 
+            this.txtBlock.Location = new System.Drawing.Point(104, 69);
+            this.txtBlock.Name = "txtBlock";
+            this.txtBlock.Size = new System.Drawing.Size(120, 20);
+            this.txtBlock.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Nazwa";
+            // 
+            // btCancel
+            // 
+            this.btCancel.Location = new System.Drawing.Point(23, 171);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(75, 23);
+            this.btCancel.TabIndex = 13;
+            this.btCancel.Text = "Anuluj";
+            this.btCancel.UseVisualStyleBackColor = true;
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(186, 171);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(75, 23);
+            this.btAdd.TabIndex = 12;
+            this.btAdd.Text = "Dodaj";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click_1);
+            // 
+            // Block_Add
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.txtBlock);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btCancel);
+            this.Controls.Add(this.btAdd);
+            this.Name = "Block_Add";
             this.Text = "Block_add";
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.BindingSource userBindingSource;
+        private DBDataSet dBDataSet;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private DBDataSetTableAdapters.UserTableAdapter userTableAdapter;
+        private System.Windows.Forms.BindingSource userBindingSource1;
+        private System.Windows.Forms.TextBox txtBlock;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.Button btAdd;
     }
 }
--- Visual Project/Forms/Block_add.cs
+++ Visual Project/Forms/Block_add.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace MTGCM.Forms
 {
     public partial class Block_Add : Form
     {
+        string connString = "Data Source = DB.db; Version = 3";
+
         public Block_Add()
         {
             InitializeComponent();
+
+        }
+
+        private void btAdd_Click_1(object sender, EventArgs e)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(connString))
+            {
+                conn.Open();
+                SQLiteCommand command = new SQLiteCommand(conn);
+                command.CommandText = @"
+                                INSERT INTO Block (name)
+                                VALUES (@name)
+                                ";
+
+                command.Parameters.Add(new SQLiteParameter("@name", txtBlock.Text));
+
+
+                command.ExecuteNonQuery();
+                conn.Close();
+
+                this.Close();
+            }
         }
     }
 }
