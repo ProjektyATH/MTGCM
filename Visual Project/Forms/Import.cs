19e4970e09d209504dce4ee40dc074092b4d8bae
marcin009988 <marcin009988@users.noreply.github.com>
2016-01-07 13:04:54 +0100

    import  SetList

--- Visual Project/Forms/Import.cs
+++ Visual Project/Forms/Import.cs
@@ -27,13 +27,38 @@ namespace MTGCM.Forms
             openFileDialog1.ShowDialog();
             textBox1.Text = path = openFileDialog1.FileName;
             textBox2.Text = text = System.IO.File.ReadAllText(@path);
-
-            List<JClasses.JSet> sets = JsonConvert.DeserializeObject<List<JClasses.JSet>>(text);
-
-            foreach (var s in sets)
+           
+            switch (checkedListBox1.SelectedIndex)
             {
-                
+                case 0:
+                    List<JClasses.JSet> sets = JsonConvert.DeserializeObject<List<JClasses.JSet>>(text);
+                    using (var db = new DBEntities())
+                    {
+                        var S = from s in db.Sets
+                                select s;
+
+
+                        foreach (var s in sets)
+                        {
+                            var tmp = S.Where(c=>c.name.Contains(s.name));
+                            if (tmp == null)
+                            {
+                                Set Tmp = new Set();
+                                Tmp.abbrev = s.code;
+                                Tmp.name = s.name;
+                                Tmp.relase_date = Convert.ToDateTime(s.releaseDate);
+
+                                db.Sets.Add(Tmp);
+                                db.SaveChanges();
+
+                            }
+                        }
+                    }
+                    break;  
+                default:
+                    break;
             }
+            
             
         }
     }
